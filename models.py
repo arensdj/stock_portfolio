f0644e7901a0b315bb13f722b888b02f311ebb6a
Jasmin <jasmin.arensdorf@gmail.com>
2019-02-28 14:41:55 -0800

    Made changes.

--- models.py
+++ models.py
@@ -1,21 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime as dt
+# from datetime import datetime as dt
 from flask_migrate import Migrate
 from . import app
 
 
+# instatiated and stored db and migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class City(db.Model):
-    __tablename__ = 'cities'
+class Company(db.Model):
+    __tablename__ = 'companies'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True, unique=True)
-    zipcode = db.Column(db.String(256), index=True, unique=True)
+    symbol = db.Column(db.String(256), index=True, unique=True)
 
-    date_created = db.Column(db.DateTime, default=dt.now())
+    # date_created = db.Column(db.DateTime, default=dt.now())
 
     def __repr__(self):
-        return '<City {}-{}>'.format(self.name, self.zipcode)
+        return '<Company {}-{}>'.format(self.name, self.symbol)
