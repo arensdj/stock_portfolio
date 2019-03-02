a19191c4a9c2b2b223cd0f7e421ef77752dbf444
Jasmin <jasmin.arensdorf@gmail.com>
2019-03-02 14:35:10 -0800

    Filled in readme file and fixed html files.

--- src/routes.py
+++ src/routes.py
@@ -2,12 +2,13 @@ from flask import render_template, abort, redirect, url_for, request
 from sqlalchemy.exc import DBAPIError, IntegrityError
 from .models import Company, db
 from . import app  # same as from src import app
+from json import JSONDecodeError
 import requests
 import json
 import os
 
 
-# the @app is imported via from . import app
+# the @app is imported via 'from . import app'
 @app.route('/') # default value is ['GET']
 def home():
     """
@@ -35,36 +36,31 @@ def search_results():
 
     # instatiate a company
     company = Company(name=data['companyName'], symbol=data['symbol'])
-    print(company)
-    print(company.name, company.symbol)
 
     # 'companyName' needs to match the database column for company
-
     # peter = User.query.filter_by(username='peter').first()
-
     # result = Company.query.filter_by(symbol='symbol').first()
-    # import pdb; pdb.set_trace()
 
     try:
-      db.session.add(company)
-      db.session.commit()
-    except (DBAPIError, IntegrityError):
-      abort(400)
-         
-    # if result:
-      # print(Company.query.filter_by(symbol='symbol').first())
-    # else:
-      # import pdb; pdb.set_trace()
+        db.session.add(company)
+        db.session.commit()
 
+        return redirect(url_for('.portfolio')) # redirect the portfolio page
+    except JSONDecodeError:
+        abort(404)
+
+    # except (DBAPIError, IntegrityError):
+      # abort(400)
+         
     # how do we check that what was committed is actually committed?
     # return response.text
     # return data['symbol']
 
-    return redirect(url_for('.portfolio')) # redirect the portfolio page
+    return render_template('stocks/search.html')
 
 @app.route('/portfolio')
 def portfolio():
   # print(Company.query.all())
   # return str(Company.query.all())
 
-  return render_template('stocks/stocks.html')
+  return render_template('stocks/portfolio.html')
