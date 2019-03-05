95b86ed6a847a5cbac53ab996f2e1815277c3f87
Jasmin <jasmin.arensdorf@gmail.com>
2019-03-05 14:15:44 -0800

    More changes.

--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-# from datetime import datetime as dt
 from flask_migrate import Migrate
 from . import app
 
@@ -14,9 +13,7 @@ class Company(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True, unique=True)
-    symbol = db.Column(db.String(16), index=True, unique=True)
-
-    # date_created = db.Column(db.DateTime, default=dt.now())
+    symbol = db.Column(db.String(256), index=True, unique=True)
 
     def __repr__(self):
         return '<Company {}-{}>'.format(self.name, self.symbol)
